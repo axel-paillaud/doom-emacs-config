8e73e7566d7a2f70f0a5a47f3b13cfcce1b6bf28
axel-paillaud <contact@axelweb.fr>
2024-06-20 22:59:39 +0200

    add .gitignore, ignor custom.el

--- custom.el
+++ custom.el
@@ -3,9 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(org-agenda-files
-   '("/home/axel/org/paillesigne/baby.org" "/home/axel/org/bois.org" "/home/axel/org/book-to-read.org" "/home/axel/org/cisaille-tole.org" "/home/axel/org/compresseur.org" "/home/axel/org/davinci-resolve.org" "/home/axel/org/dev-pers.org" "/home/axel/org/hitachi-tv.org" "/home/axel/org/malette-outils.org" "/home/axel/org/mes-comptes.org" "/home/axel/org/refile.org" "/home/axel/org/reseau-etoile.org" "/home/axel/org/restauration-james-cook.org" "/home/axel/org/surveillance-camera.org" "/home/axel/org/todo.org" "/home/axel/org/video.org" "/home/axel/org/webcam.org")))
+   '("/home/shaun/org/paillesigne/baby.org" "/home/shaun/org/bois.org" "/home/shaun/org/book-to-read.org" "/home/shaun/org/cisaille-tole.org" "/home/shaun/org/compresseur.org" "/home/shaun/org/davinci-resolve.org" "/home/shaun/org/dev-pers.org" "/home/shaun/org/hitachi-tv.org" "/home/shaun/org/malette-outils.org" "/home/shaun/org/mes-comptes.org" "/home/shaun/org/refile.org" "/home/shaun/org/reseau-etoile.org" "/home/shaun/org/restauration-james-cook.org" "/home/shaun/org/surveillance-camera.org" "/home/shaun/org/todo.org" "/home/shaun/org/video.org" "/home/shaun/org/webcam.org")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
